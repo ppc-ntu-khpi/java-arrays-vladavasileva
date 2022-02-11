1dae411309b0fd39126fb01d5df7af2ff32aab16
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2022-02-11 10:56:04 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,33 @@
 package domain;
-
+/**
+ * Найголовніший клас, де виконується логіка програми
+ * @author Влада
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int Calculate(int arr[],int max_element,int max_repeated, int max_count){
+    /**
+     * оголоушємо масив розміру max_element+1 
+     */
+    int count_arr[] = new int[max_element+1];
+    /**
+     * рахуємо кількість елементів за домомогою циклу 
+     */
+    for(int i=0; i<arr.length; i++){
+            count_arr[arr[i]]++;
+        }
+    /**
+     * знаходимо кількість елементів з найбільшим значенням 
+     */
+        for(int i=0; i<arr.length; i++){
+            if(count_arr[arr[i]] > max_count){
+                max_count = count_arr[arr[i]];
+                max_repeated = arr[i];
+            }
+}
+        /** 
+         * повертаємо значення 
+         */
+        return (int) max_repeated; 
     }
+    
 }
