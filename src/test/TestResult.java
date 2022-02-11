bfb10767de4f762f9a0eb9696627cd5a00424564
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2022-02-11 11:02:57 +0200

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,22 +2,22 @@ package test;
 
 import domain.Exercise;
 /**
- * РљР»Р°СЃ, Р·Р° РґРѕРїРѕРјРѕРіРѕСЋ СЏРєРѕРіРѕ РјРё РІРёРІРѕРґРёРјРѕ СЂРµР·СѓР»СЊС‚Р°С‚ 
- * @author Р’Р»Р°РґР°
+ * Клас, за допомогою якого ми виводимо результат 
+ * @Влада
  */
 public class TestResult {
 
     public static void main(String[] args) {
            /**
-            * РѕРіРѕР»РѕС€РµРЅРЅСЏ РјР°СЃРёРІСѓ
+            *  оголошення масиву
             */
         int arr[]={7,0,0,1,4,0,7,2};
         int max_element = 8;
         int max_repeated = Integer.MIN_VALUE;
         int max_count = -1;
-        //**
-        * 
-        */
-        System.out.println( "Р§РёСЃР»Рѕ, СЏРєРµ РїРѕРІС‚РѕСЂСЋС”С‚СЊСЃСЏ РЅР°Р№Р±С–Р»СЊС€Сѓ РєС–Р»СЊРєС–СЃС‚СЊ СЂР°Р·С–РІ: " + Exercise.Calculate (arr, max_element,max_repeated, max_count));  
+        /**
+         * Виведення на екран числа, яке повторюєтоься у масиві найбільшу кількість разів
+         */
+        System.out.println( "Число, яке повторюєтоься у масиві найбільшу кількість разів : " + Exercise.Calculate (arr, max_element,max_repeated, max_count));  
     }
 }
