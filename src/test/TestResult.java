d60bd5f0f8ea9d0fa6b5eb6aa266a4f47b3e43bc
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2022-02-11 11:01:27 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -9,13 +9,15 @@ public class TestResult {
 
     public static void main(String[] args) {
            /**
-            * оголомення масиву
+            * оголошення масиву
             */
         int arr[]={7,0,0,1,4,0,7,2};
         int max_element = 8;
         int max_repeated = Integer.MIN_VALUE;
         int max_count = -1;
-        //output the most repeated element along with the count
+        //**
+        * 
+        */
         System.out.println( "Число, яке повторюється найбільшу кількість разів: " + Exercise.Calculate (arr, max_element,max_repeated, max_count));  
     }
 }
